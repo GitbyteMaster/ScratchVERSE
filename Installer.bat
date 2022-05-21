a31477e31b09816171830b065f669110093c7beb
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-05-21 15:09:31 -0400

    Create Installer.bat

---
+++ Installer.bat
@@ -0,0 +1,9 @@
+echo Installing..
+mkdir /Users/$USER/Desktop/ScratchVERSE
+echo  > /Users/$USER/Desktop/ScratchVERSE/getcurl
+echo > /Users/$USER/Desktop/ScratchVERSE/home.svg
+echo > /Users/$USER/Desktop/ScratchVERSE/userinfo
+curl https://raw.githubusercontent.com/GitbyteMaster/ScratchVERSE/main/home-top.svg > /Users/$USER/Desktop/ScratchVERSE/home-top.svg
+curl https://raw.githubusercontent.com/GitbyteMaster/ScratchVERSE/main/main.py > /Users/$USER/Desktop/ScratchVERSE/main.py
+echo You've finished the installion! Thanks for installing ScratchVERSE!
+pause
