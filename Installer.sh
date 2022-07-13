64521aaa08dddeaac771644c2057790f9a387120
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-07-13 09:27:00 -0400

    Update and rename Installer.bat to Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -6,4 +6,3 @@ echo > /Users/$USER/Desktop/ScratchVERSE/userinfo
 curl https://raw.githubusercontent.com/GitbyteMaster/ScratchVERSE/main/home-top.svg > /Users/$USER/Desktop/ScratchVERSE/home-top.svg
 curl https://raw.githubusercontent.com/GitbyteMaster/ScratchVERSE/main/main.py > /Users/$USER/Desktop/ScratchVERSE/main.py
 echo You've finished the installion! Thanks for installing ScratchVERSE!
-pause
